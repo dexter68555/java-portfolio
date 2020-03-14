b3bc3fccc4ebba0646733b081677620fc3c91b01
dexter68555 <46341564+dexter68555@users.noreply.github.com>
2020-03-15 00:00:35 +0800

    Update Calculator.java

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -6,7 +6,7 @@ import java.lang.Math;
 
 public class Calculator {
 
-    private static final int WINDOW_WIDTH = 410;
+    private static final int WINDOW_WIDTH = 400;
     private static final int WINDOW_HEIGHT = 600;
     private static final int BUTTON_WIDTH = 80; // Button width
     private static final int BUTTON_HEIGHT = 70; // Button height
